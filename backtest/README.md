cd536f6f659e82a04c447a6ff6f8533db8030398
Jon O'Reilly del Cerro <jon.oreilly@easybell.de>
2025-06-07 18:06:27 +0200

    feat: add

--- backtest/README.md
+++ backtest/README.md
@@ -14,7 +14,44 @@ Then install all the dependencies:
 npm ci
 ```
 
+To use a database as the source of the data you'll need to configure the [env](./.env) file with the following fields:
+
+```
+DB_USER=xxx
+DB_PASSWORD=xxx
+DB_HOST=localhost
+DB_PORT=5432
+DB_NAME=bitcoin_db
+```
+
 ### Run the backtest
+
+Before running the backtest, configure your [env](./.env) file with the settings for the backtest you want to run. Here's an example:
+
+```
+# First day the algorithm will calculate
+START_DATE=2020-01-01
+# Last day the algorithm will calculate
+END_DATE=2020-01-02
+# Run or don't run the "hold" algorithm
+SHOULD_RUN_ALGORITHM_HOLD=true
+# Run or don't run the "moving average" algorithm
+SHOULD_RUN_ALGORITHM_MEAN_REVERSION=true
+# Run or don't run the "moving average volume compensated" algorithm
+SHOULD_RUN_ALGORITHM_MEAN_REVERSION_VOLUME_COMPENSATED=true
+# Run or don't run the "mean reversion" algorithm
+SHOULD_RUN_ALGORITHM_MOVING_AVERAGE=true
+# Run or don't run the "mean reversion volume compensated" algorithm
+SHOULD_RUN_ALGORITHM_MOVING_AVERAGE_VOLUME_COMPENSATED=true
+# The days the long term average is calculated for, for the "moving average" algorithm
+MOVING_AVERAGE_LONG_TERM_DAYS=0.1
+# The days the short term average is calculated for, for the "moving average" algorithm
+MOVING_AVERAGE_SHORT_TERM_DAYS=0.025
+# The days the mean is calculated for, for the "mean reversion" algorithm
+MEAN_REVERSION_SCOPE_DAYS=1
+# The maximum amount of dollar value the bot is allowed to trade every day
+TRADE_DOLLAR_MAX_AMOUNT=100_000
+```
 
 The following command will run the file [./src/index.ts](./src/index.ts):
 
---
+++ backtest/src/utils/settings.ts
@@ -0,0 +1,76 @@
+import { config } from 'dotenv';
+
+config();
+
+function getEnvValue(name: string) {
+  return process.env[name];
+}
+
+const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+function parseDate(name: string) {
+  const dateString = getEnvValue(name);
+
+  if (!dateString || !dateRegex.test(dateString)) {
+    throw new Error(`Incorrect env date format: ${name} = ${dateString}`);
+  }
+
+  return new Date(dateString);
+}
+
+function parseNumber(name: string) {
+  const numberString = getEnvValue(name);
+
+  const number = Number(numberString);
+
+  if (isNaN(number)) {
+    throw new Error(`Incorrect env number format: ${name} = ${numberString}`);
+  }
+
+  return number;
+}
+
+function parseBoolean(booleanString?: string) {
+  return booleanString === 'true';
+}
+
+console.log('Reading env settings...');
+
+export const settings = {
+  /** First day the algorithm will calculate */
+  startDate: parseDate('START_DATE'),
+  /** Last day the algorithm will calculate */
+  endDate: parseDate('END_DATE'),
+  /** Run or don't run the "hold" algorithm */
+  shouldRunAlgorithmHold: parseBoolean('SHOULD_RUN_ALGORITHM_HOLD'),
+  /** Run or don't run the "moving average" algorithm */
+  shouldRunAlgorithmMovingAverage: parseBoolean(
+    'SHOULD_RUN_ALGORITHM_MOVING_AVERAGE',
+  ),
+  /** Run or don't run the "moving average volume compensated" algorithm */
+  shouldRunAlgorithmMovingAverageVolumeCompensated: parseBoolean(
+    'SHOULD_RUN_ALGORITHM_MOVING_AVERAGE_VOLUME_COMPENSATED',
+  ),
+  /** Run or don't run the "mean reversion" algorithm */
+  shouldRunAlgorithmMeanReversion: parseBoolean(
+    'SHOULD_RUN_ALGORITHM_MEAN_REVERSION',
+  ),
+  /** Run or don't run the "mean reversion volume compensated" algorithm */
+  shouldRunAlgorithmMeanReversionVolumeCompensated: parseBoolean(
+    'SHOULD_RUN_ALGORITHM_MEAN_REVERSION_VOLUME_COMPENSATED',
+  ),
+  /** The days the long term average is calculated for, for the "moving average" algorithm */
+  movingAveragelongTermDays: parseNumber('MOVING_AVERAGE_LONG_TERM_DAYS'),
+  /** The days the short term average is calculated for, for the "moving average" algorithm */
+  movingAverageShortTermDays: parseNumber('MOVING_AVERAGE_SHORT_TERM_DAYS'),
+  /** The days the mean is calculated for, for the "mean reversion" algorithm */
+  meanReversionScopeDays: parseNumber('MEAN_REVERSION_SCOPE_DAYS'),
+  /** The maximum amount of dollar value the bot is allowed to trade every day */
+  tradeDollarMaxAmount: parseNumber('TRADE_DOLLAR_MAX_AMOUNT'),
+  /** Technical setting: this specifies for how many days before the start date and after the end date should we fetch the data */
+  marginDays: 2,
+};
+
+console.log('Settings read:');
+console.log(settings);
+console.log();
